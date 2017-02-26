43dbba18afcd8a9f8494dfcf11abe1e95a37991a
Cesar Velarde <cevegaju@users.noreply.github.com>
2017-02-25 23:17:45 -0500

    Update Customer.cs

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,6 @@
 namespace Models
 {
-    public partial class Customer
+    public class Customer
     {
         public int Id { get; set; }                
         public string FirstName { get; set; }        
